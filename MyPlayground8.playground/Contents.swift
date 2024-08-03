dc71c55ad9875eccbe0e9e15ac1311451879d87f
Mohratu <Mohraturki.23@gmail.com>
2024-08-03 21:29:57 +0300

    Initial Commit

---
+++ MyPlayground8.playground/Contents.swift
@@ -0,0 +1,21 @@
+let studentGrade = 100
+
+if studentGrade > 90 {
+    print("Excellent")
+}
+
+else if studentGrade > 80 {
+    print("Vary Good")
+}
+
+else if studentGrade > 70 {
+    print("Good")
+}
+
+else if studentGrade > 60 {
+    print("Pass")
+}
+
+else {
+    print("beiow")
+}
